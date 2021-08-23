2d63aa5c897ddf84d3dc37595de0918e0defdfca
talisilva <talissaagtha@gmail.com>
2021-08-23 16:42:38 -0300

    uptarefa2fb

--- Furando a bolha_mysql_create.sql
+++ Furando a bolha_mysql_create.sql
@@ -1,25 +1,28 @@
 CREATE TABLE `tb_postagem` (
-	`id` bigint NOT NULL AUTO_INCREMENT,
+	`id`  NOT NULL,
 	`titulo` varchar(255) NOT NULL,
 	`texto` varchar(5000) NOT NULL,
 	`data` TIMESTAMP NOT NULL,
 	`alteracao_data` TIMESTAMP NOT NULL,
-	`tema_id` bigint NOT NULL,
-	`usuario_id` bigint NOT NULL,
+	`tema_id`  NOT NULL,
+	`usuario_id`  NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `tb_tema` (
-	`id` bigint NOT NULL AUTO_INCREMENT,
+	`id`  NOT NULL,
 	`descricao` varchar(1000) NOT NULL,
+	`tema` varchar(255) NOT NULL,
+	`palavra chave` varchar(255) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `tb_usuario` (
-	`id` bigint NOT NULL AUTO_INCREMENT,
+	`id`  NOT NULL,
 	`nome` varchar(255) NOT NULL,
 	`usuario` varchar(255) NOT NULL,
 	`senha` varchar(500) NOT NULL,
+	`tipo usuario` varchar(255) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
